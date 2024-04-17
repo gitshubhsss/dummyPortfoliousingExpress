dc8eab75384085cc82d1031c854df7260fe67f5b
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-17 22:27:29 +0530

    adding resume text

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+let btn=document.querySelector("button");
+let p=document.querySelector("#para");
+
+btn.addEventListener("click",()=>{
+    p.innerText="Nothing to see bro theres lots to learn";
+})
